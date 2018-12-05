13481c6ea5c601f7bce1622239683adfe6fb3638
Hanyauku <kochneva.p@gmail.com>
2018-12-05 11:07:19 +0100

    css fixed

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -11,26 +11,26 @@ export default class NavBar extends React.Component {
         return (
             <div>
 
-            <div class="row">
-                    <div class="col-md-3 widget text-4 widget_text" id="footer">
+            <div className="row">
+                    <div className="col-md-3 widget text-4 widget_text" id="footer">
                         <h4>Contact</h4><hr/>
-                        <div class="textwidget">
+                        <div className="textwidget">
                         <p><a href="mailto:team@femaleventures.nl" target="_blank" rel="noopener">team@femaleventures.nl</a></p>
                         <p>&nbsp;</p>
                         </div>
                     </div>
-                    <div class="col-md-3 widget text-4 widget_text" id="footer">
+                    <div className="col-md-3 widget text-4 widget_text" id="footer">
                         <h4>Follow us</h4><hr/>
-                        <div class="textwidget">
-                            <p><span class="fa fa-linkedin-square" >&nbsp;</span><a href="https://www.linkedin.com/organization/14974484/" target="_blank" rel="noopener">Link us on Linkedin</a><br/>
-                            <span class="fa fa-facebook-square">&nbsp;</span><a href="https://www.facebook.com/femaleventures/" target="_blank" rel="noopener">Like us on Facebook</a><br/>
-                            <span class="fa fa-twitter">&nbsp;</span><a href="https://twitter.com/FemVentures" target="_blank" rel="noopener">Follow us on Twitter</a></p>
+                        <div className="textwidget">
+                            <p><span className="fa fa-linkedin-square" >&nbsp;</span><a href="https://www.linkedin.com/organization/14974484/" target="_blank" rel="noopener">Link us on Linkedin</a><br/>
+                            <span className="fa fa-facebook-square">&nbsp;</span><a href="https://www.facebook.com/femaleventures/" target="_blank" rel="noopener">Like us on Facebook</a><br/>
+                            <span className="fa fa-twitter">&nbsp;</span><a href="https://twitter.com/FemVentures" target="_blank" rel="noopener">Follow us on Twitter</a></p>
                         </div>
                     </div>
 
-                    <div class="col-md-3 widget text-4 widget_text" id="footer">
+                    <div className="col-md-3 widget text-4 widget_text" id="footer">
                         <h4>Subscribe to Our Newsletter</h4><hr/>
-                        <div class="textwidget">
+                        <div className="textwidget">
                         <form>
                             <FormGroup>
                             <Label for="exampleEmail">Email*</Label>
@@ -41,9 +41,9 @@ export default class NavBar extends React.Component {
                         </div>
                     </div>
 
-                    <div class="col-md-3 widget nav_menu-2 widget_nav_menu"id="footer">
+                    <div className="col-md-3 widget nav_menu-2 widget_nav_menu"id="footer">
                         <h4>Information</h4><hr/>
-                        <div class="menu-footer-menu-container">
+                        <div className="menu-footer-menu-container">
                             <ul>
                                 <li><a href="https://femaleventures.nl/terms-and-conditions/">Terms and Conditions</a></li><hr/>
                                 <li><a href="https://femaleventures.nl/privacy-main/">Privacy</a></li>
