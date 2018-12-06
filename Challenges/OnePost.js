273968ac2ecce56cd27d89d144e53d687ced36d3
bhzads <40757770+bhzads@users.noreply.github.com>
2018-12-06 16:41:10 +0100

    Add files via upload

--- Challenges/OnePost.js
+++ Challenges/OnePost.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 import {Link} from 'react-router-dom';
 import {Card,Row,Col,CardHeader,CardTitle,CardText,Button,CardFooter  } from 'reactstrap';
-import './onepost.css';
+import './css/onepost.css';
 
 export default class Posts extends Component {
     constructor(props) {
@@ -23,23 +23,19 @@ export default class Posts extends Component {
         body = body.slice(0, 180);
         return (
         <div key={this.props.data._id}>
-        
-        <Card body id="card">
-            <CardTitle className="CardTitle" tag="h4">{title}</CardTitle>
-            <CardText>{body}<Link to={`/challenge/${_id}`}>...Read more</Link></CardText>
-            <footer className="CardFooter">
-                <div className="postfooterBy">
-                by: <Link to={`/friend/${this.props.data.user._id}`}>{firstName} {lastName}</Link>..at {createdAt}
-                </div>
-                <div className="postfooterComment">
-                <Link to={`/challenge/${_id}`}>{this.state.count} Comments</Link>
-                </div>
-            </footer >
-        </Card><br/>
-
-
+            <Card body id="card">
+                <CardTitle className="CardTitle" tag="h4">{title}</CardTitle>
+                <CardText>{body}<Link to={`/challenge/${_id}`}>...Read more</Link></CardText>
+                <footer className="CardFooter">
+                    <div className="postfooterBy">
+                    by: <Link to={`/friend/${this.props.data.user._id}`}>{firstName} {lastName}</Link>..at {createdAt}
+                    </div>
+                    <div className="postfooterComment">
+                    <Link to={`/challenge/${_id}`}>{this.state.count} Comments</Link>
+                    </div>
+                </footer >
+            </Card>
         </div>
         );
     };
 }
-
