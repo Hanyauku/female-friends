c1449e6216f2634d38c56658d851235df7e20472
Kasiban <38992902+Kasiban@users.noreply.github.com>
2018-12-06 07:42:51 -0800

    add list of top 5 members

--- src/components/Main/TopMembers.js
+++ src/components/Main/TopMembers.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import OneUser from '../Friends/OneUser';
+import {Link} from 'react-router-dom';
+import { Card, CardBlock, CardTitle } from 'reactstrap';
 
 class TopMembers extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+            top: []
+        }
+    }
+
+    componentDidMount() {
+        Axios.get('http://localhost:8000/api/user/top').then(res => this.setState( {top: res.data} ));
+        console.log('Hello');
+    };    
   render() {
     return (
+        
         <div>
-            Top Members
+            <Card>
+                <CardTitle>Top Members</CardTitle>
+                    <ul>
+                        <li>
+                        <Link to={`/friend/${this.props.data}`}>
+                            {this.state.top.map(user => (
+						      <OneUser data={user}/> 
+				            ))}
+                         </Link>
+                        </li>
+                    </ul>
+            </Card>
         </div>
-    );
+     );
   }
 }
 
